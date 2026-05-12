385457e5e483d3c75ea46332371fb45f4016f35e
MyGit-demo <cibaca123@gmail.com>
2026-05-11 19:34:38 -0500

    mainjs

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/React_Apps/",
+ // base:"/React_Apps/",
+  base: "/", 
   plugins: [react()],
 })
